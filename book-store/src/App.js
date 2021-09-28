696b4c5d9fba5fcacf226d40e4eb77c006481101
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-09-28 16:22:43 +0200

    Navbar

---
+++ book-store/src/App.js
@@ -0,0 +1,15 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import MyNavbar from './components/MyNavbar'
+
+function App() {
+
+  return (
+    <div>
+      <MyNavbar brand="StriveBooks" />
+     
+    </div>
+  )
+}
+
+export default App
---
+++ book-store/src/components/MyNavbar.jsx
@@ -0,0 +1,19 @@
+/* import Navbar from 'react-bootstrap/Navbar'
+import Nav from 'react-bootstrap/Nav' */
+import { Navbar, Nav } from 'react-bootstrap'
+
+const MyNavBar = (props) => (
+    <Navbar collapseOnSelect expand="md" bg="light" variant="light">
+        <Navbar.Brand href="#home">{props.brand}</Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+                <Nav.Link href="#features">Home</Nav.Link>
+                <Nav.Link href="#pricing">About</Nav.Link>
+                <Nav.Link href="#pricing">Browse</Nav.Link>
+            </Nav>
+        </Navbar.Collapse>
+    </Navbar>
+)
+
+export default MyNavBar
