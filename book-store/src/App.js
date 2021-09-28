045723c08be2f5f44963167a19e1da2bdc92b629
Rafael Ferreira <rafaelnevesf@gmail.com>
2021-09-28 16:59:00 +0200

    Jumbotron/footer

--- book-store/src/App.js
+++ book-store/src/App.js
@@ -1,13 +1,17 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import MyNavbar from './components/MyNavbar'
+import MyFooter from './components/MyFooter'
+import MyJumbotron from './components/MyJumbotron'
+
 
 function App() {
 
   return (
     <div>
       <MyNavbar brand="StriveBooks" />
-     
+      <MyFooter/>
+      <MyJumbotron/>
     </div>
   )
 }
---
+++ book-store/src/components/MyFooter.jsx
@@ -0,0 +1,16 @@
+import Card from 'react-bootstrap/Card'
+
+const MyFooter = () => (
+    <Card className="text-center fixed-bottom">
+        
+        <Card.Body>
+            <Card.Text>
+                Address: Frankfurter Allee 60, 10247 Berlin
+            </Card.Text>
+            
+        </Card.Body>
+        <Card.Footer className="text-muted">Made in Berlin</Card.Footer>
+    </Card>
+)
+
+export default MyFooter
